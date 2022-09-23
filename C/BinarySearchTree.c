6bfedcc18c1b23bf57d28baddcb7aa9c0b1f94b5
Ian Young <ianyoung@SUU.EDU>
2022-09-23 12:23:00 -0600

    Added Math

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <math.h>
+
+int numOfBST(int);
 
 int main() {
+    
     return 0;
+}
+
+numOfBST(int key) {
+    int catalanNumber = factorial(2 * key)/(factorial(key + 1) * factorial(key)); 
+    return catalanNumber;
 }
